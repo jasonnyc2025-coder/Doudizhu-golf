5812f1bc4c21272c199b149f118f113b6d93346a
jasonnyc2025-coder <jason.nyc2025@gmail.com>
2026-05-22 15:25:10 +0700

    Add files via upload

---
+++ sw-1.js
@@ -0,0 +1,33 @@
+const CACHE = 'ddz-golf-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// 网络优先，确保每次打开拿最新版；离线时用缓存
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+      return resp;
+    }).catch(() => caches.match(e.request))
+  );
+});
